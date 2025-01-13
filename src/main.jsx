1fd531ac322f2d57695f58f72356a0f8074b8e16
Khaly_Ruot <63812639+khalyruot@users.noreply.github.com>
2025-01-13 10:00:44 +0700

    add Image.jsx for fetching data

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,26 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import routes from "./routes";
+import App from "./App";
+import Profile from "./Profile";
+import ErrorPage from "./ErrorPage";
+import Image from "./Image";
 
-const router = createBrowserRouter(routes);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/image",
+    element: <Image />
+  },
+  {
+    path: "profile/:name",
+    element: <Profile />,
+  }
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
