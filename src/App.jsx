205b5b696307bc6b9c88202267ee08b180415212
Khaly_Ruot <63812639+khalyruot@users.noreply.github.com>
2025-01-14 10:28:14 +0700

    add Link instead of a href

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const App = () => {
   return (
     <div>
@@ -6,7 +8,7 @@ const App = () => {
       <nav>
         <ul>
           <li>
-            <a href="home">Home page</a>
+            <Link to="home">Home page</Link>
           </li>
         </ul>
       </nav>
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <div>
-      <h1>Hello from profile page!</h1>
+      <h1>Hello from Home page!</h1>
       <p>So, how are you?</p>
     </div>
   );
