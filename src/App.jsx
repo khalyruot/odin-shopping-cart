acebf8051dee918e0f5ba4a1ff18c17ba1d7bf4b
Khaly_Ruot <63812639+khalyruot@users.noreply.github.com>
2025-01-20 12:23:02 +0700

    add button

--- src/App.jsx
+++ src/App.jsx
@@ -5,41 +5,41 @@ const App = () => {
   return (
     <div>
        <div id="top_menu_fix">
-    <nav class="navbar navbar-inverse navbar-fixed-top" >
-      <div id="navbar">
-        <div class="container-fluid">
-          <div >
-            <a id="Kong_Fu_Logo" class="navbar-brand"></a>
-          </div>
+            <nav class="navbar navbar-inverse navbar-fixed-top" >
+                        <div id="navbar">
+                          <div class="container-fluid">
+                            <div >
+                              <a id="Kong_Fu_Logo" class="navbar-brand"></a>
+                            </div>
+                          </div>
+                          
+                          <div>
+                            <ul class="nav navbar-nav" id="button_nav">
+                              <li><a href="#"><button id="homeTab">Home</button></a></li>
+                              <li><a href="#"><button id="menuTab">Shop</button></a></li>
+                            </ul>
+                          </div>
+                        </div>
+                
+            </nav>
         </div>
-        
-        <div>
-          <ul class="nav navbar-nav" id="button_nav">
-            <li><a href="#"><button id="homeTab">Home</button></a></li>
-            <li><a href="#"><button id="menuTab">Shop</button></a></li>
-          </ul>
-        </div>
-      </div>
-        
-    </nav>
-  </div>
 
-      <nav>
-        <ul>
-          <li>
-            <h1><Link to="home">Home page</Link></h1> 
-          </li>
-          <li>
-            <h1><Link to="home/shoppingcart">Sopping Cart</Link></h1> 
-          </li>
-          <li>
-            <h1><Link to="home/aboutpage">About page</Link></h1>
-          </li>
-          <li>
-            <h1><Link to="home/image">Image</Link></h1>
-          </li>
-        </ul>
-      </nav>
+        <nav>
+          <ul>
+            <li>
+              <h1><Link to="home">Home page</Link></h1> 
+            </li>
+            <li>
+              <h1><Link to="home/shoppingcart">Sopping Cart</Link></h1> 
+            </li>
+            <li>
+              <h1><Link to="home/aboutpage">About page</Link></h1>
+            </li>
+            <li>
+              <h1><Link to="home/image">Image</Link></h1>
+            </li>
+          </ul>
+        </nav>
     </div>
   );
 };
--- src/Home.jsx
+++ src/Home.jsx
@@ -4,8 +4,25 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <div>
-      <h1>Hello from Home page!</h1>
-      <p>So, how are you?</p>
+     <div id="top_menu_fix">
+            <nav class="navbar navbar-inverse navbar-fixed-top" >
+                        <div id="navbar">
+                          <div class="container-fluid">
+                            <div >
+                              <a id="Kong_Fu_Logo" class="navbar-brand"></a>
+                            </div>
+                          </div>
+                          
+                          <div>
+                            <ul class="nav navbar-nav" id="button_nav">
+                              <li><a href="#"><button id="homeTab">Home</button></a></li>
+                              <li><a href="#"><button id="menuTab">Shop</button></a></li>
+                            </ul>
+                          </div>
+                        </div>
+                
+            </nav>
+        </div>
       <Link to="/">Click here to go back Home</Link>
       <Outlet />
     </div>
